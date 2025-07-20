0e65f00138a084c64138c06af43ef34cd968a668
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2025-07-20 20:31:40 +0200

    created a cli with clikt

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,3 +1,12 @@
+plugins {
+    application
+}
+
 dependencies {
     implementation(project(":core"))
+    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+}
+
+application {
+    mainClass = "de.termitehuegel.sudoku.cli.CliApplicationKt"
 }
---
+++ cli/src/main/kotlin/de/termitehuegel/sudoku/cli/commands/SudokuCli.kt
@@ -0,0 +1,7 @@
+package de.termitehuegel.sudoku.cli.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class SudokuCli: CliktCommand() {
+    override fun run() = Unit
+}
